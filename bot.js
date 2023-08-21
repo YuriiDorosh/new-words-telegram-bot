9b13c852b1656af80393fef9ea075af056a3e734
Yurii <yuriydorosh2005@gmail.com>
2023-08-21 16:28:56 +0300

    UPDATE: main file

--- bot.js
+++ bot.js
@@ -1,3 +1,15 @@
+const { Telegraf } = require('telegraf');
+const { addWordHandler, getWordsHandler } = require('./handlers');
+const { initializeDatabase } = require('./database');
+
 require('dotenv').config();
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+
+initializeDatabase();
+
+bot.start(ctx => ctx.reply('Welcome! Use /addword to add a word and /showwords to view your words.'));
+bot.hears(/\/addword (.+)/, (ctx) => addWordHandler(ctx));
+bot.command('showwords', (ctx) => getWordsHandler(ctx, bot));
+
+bot.launch();
