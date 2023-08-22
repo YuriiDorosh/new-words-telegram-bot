b7e7315c6cd014ce081b91ac3c40fd9b0e9d2cdc
Yurii <yuriydorosh2005@gmail.com>
2023-08-22 17:32:04 +0300

    UPDATE: import and use help handler

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const { Telegraf } = require('telegraf');
-const { addWordHandler, getWordsHandler, deleteWordHandler,getRandomWordHandler } = require('./handlers');
+const { addWordHandler, getWordsHandler, deleteWordHandler,getRandomWordHandler, helpHandler } = require('./handlers');
 const { initializeDatabase } = require('./database');
 
 require('dotenv').config();
@@ -9,6 +9,7 @@ const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 initializeDatabase();
 
 bot.start(ctx => ctx.reply('Welcome! Use /addword to add a word and /showwords to view your words.'));
+bot.command('help', helpHandler);
 bot.hears(/\/addword (.+)/, (ctx) => addWordHandler(ctx));
 bot.hears(/\/delete_word (.+)/, (ctx) => deleteWordHandler(ctx));
 bot.command('showwords', (ctx) => getWordsHandler(ctx, bot));
